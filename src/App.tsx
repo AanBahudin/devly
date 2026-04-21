47ced05451d8712bc6203ad8ced8007225edd67d
ivansabila <ivanadiyatmasabila@gmail.com>
2026-04-21 14:06:17 +0800

    make responsive why us section, closes #10

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import Button from './components/Button';
 import Navbar from './components/Navbar';
 import heroImageBorder from './assets/hero-image-border.svg';
 import heroImageLogo from './assets/hero-image-logo.svg';
+import Card from './components/Card';
+import cardImage1 from './assets/card-image-1.png';
+import cardImage2 from './assets/card-image-2.png';
+import cardImage3 from './assets/card-image-3.png';
+import cardImage4 from './assets/card-image-4.png';
 
 const App = () => {
   return (
     <div className="min-h-dvh bg-background">
       <Navbar />
-      <div className="mt-16 mx-5 pt-8 flex flex-col overflow-hidden">
+      <div className="mt-16 mx-5 pt-8 flex flex-col items-center gap-8 overflow-hidden">
         <section className="flex flex-col items-center gap-12">
           <div className="flex flex-col items-center gap-8 z-10 mx-auto md:max-w-lg lg:max-w-3xl">
             <div className="w-fit px-3 py-1 flex gap-2 items-center border border-border rounded-lg bg-accent/5">
@@ -18,7 +23,7 @@ const App = () => {
               </span>
               <p className="text-xs font-medium text-accent">New launch</p>
             </div>
-            <div className="mx-2 flex flex-col gap-4 text-center">
+            <div className="flex flex-col gap-4 text-center">
               <h1 className="text-title md:text-6xl font-medium text-foreground">
                 Satu Ekosistem, Ribuan Peluang Nyata
               </h1>
@@ -47,6 +52,46 @@ const App = () => {
               className="absolute top-[53.5%] left-[53.5%] -translate-1/2 z-10 size-40 md:size-60 lg:size-80"
               src={heroImageLogo}
               alt="Hero image logo"
+            />
+          </div>
+        </section>
+        <section className="flex flex-col gap-12 md:max-w-3xl lg:max-w-6xl">
+          <div className="flex flex-col gap-8">
+            <div className="w-fit px-3 py-1 border border-border rounded-r-full rounded-tl-full bg-accent/5">
+              <p className="text-sm font-medium text-accent">Why Us?</p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h1 className="text-title md:text-6xl font-medium text-foreground">
+                Tidak Hanya Platform, Kami Merancang Ekosistem
+              </h1>
+              <p className="font-medium text-muted-foreground">
+                Kami membangun infrastruktur digital yang menjadi fondasi utama
+                pertumbuhan bisnis Anda.
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <Card
+              image={cardImage1}
+              title="Perlindungan Ekosistem"
+              description="Setiap platform yang kami bangun menggunakan standar 'By Devly'
+              yang memastikan keamanan, skalabilitas dan kemudahan penggunaan
+              platform."
+            />
+            <Card
+              image={cardImage2}
+              title="Desain Interaktif"
+              description="Kami merancang desain navigasi yang intuitif, responsivitas sempurna, dan berfokus pada pengalaman pengguna."
+            />
+            <Card
+              image={cardImage3}
+              title="Konsultan Strategi Produk"
+              description="Kami tidak hanya mengeksekusi teknis, kami dapat membantu Anda dari validasi konsep hingga integrasi layanan sehingga tujuan utama Anda dapat tercapai."
+            />
+            <Card
+              image={cardImage4}
+              title="Laporan Real-time"
+              description="Ubah data mentah menjadi wawasan jelas dalam hitungan detik untuk pengambilan keputusan cerdas berbasis data real-time."
             />
           </div>
         </section>
---
+++ src/components/Card.tsx
@@ -0,0 +1,30 @@
+interface CardProps {
+  image: string;
+  title: string;
+  description: string;
+  alt: string;
+  className?: string;
+}
+
+const Card = ({ image, title, description, alt, className }: CardProps) => {
+  return (
+    <div
+      className={`h-full p-1 bg-background border border-border/50 rounded-xl ${className}`}
+    >
+      <div className="relative size-full flex items-center justify-center rounded-lg backdrop-blur-lg">
+        <div className="absolute size-full bg-white rounded-lg blur-[2px]" />
+        <div className="h-full p-5 flex flex-col gap-8 z-10">
+          <img className="object-cover" src={image} alt={alt} />
+          <div className="flex flex-col gap-2">
+            <h1 className="text-[28px] font-medium text-foreground">{title}</h1>
+            <p className="font-medium text-muted-foreground/80">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
